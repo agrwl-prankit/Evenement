282fd93764de71b1360939560008e4245d8ea395
agrwl-prankit <agrwnlprankit77@gmail.com>
2021-03-01 19:43:14 +0530

    add activity_setting.xml

--- app/src/main/java/com/prankit/evenement/activities/SettingActivity.java
+++ app/src/main/java/com/prankit/evenement/activities/SettingActivity.java
@@ -1,16 +1,32 @@
 package com.prankit.evenement.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.prankit.evenement.R;
+import com.prankit.evenement.Utils.AppInfo;
+
+import io.realm.Realm;
+import io.realm.mongodb.User;
 
 public class SettingActivity extends AppCompatActivity {
+
+    AppInfo appInfo;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        Toolbar toolbar = findViewById(R.id.settingToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Setting");
+
+        Realm.init(this);
+        appInfo = new AppInfo();
+        user = appInfo.getApp().currentUser();
     }
 }
